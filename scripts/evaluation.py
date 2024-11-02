cd655bf86a9a19ab62ae910e4b6b6aa7ebcc85cc
kornkamolsampaongern <kornkamol.sampaongern@kita.earth>
2024-11-02 21:01:05 +0000

    update

--- scripts/evaluation.py
+++ scripts/evaluation.py
@@ -1,158 +1,183 @@
-import argparse
-from tools.OpenAIConnection import OpenAIConnection
-import logging 
-import os 
-import json
-import csv
-import ast
+import logging, json, csv, argparse, evaluate
 import pandas as pd
 from rouge_score import rouge_scorer
+from tools.OpenAIConnection import OpenAIConnection
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
-import evaluate
-# from deepeval.metrics import FaithfulnessMetric
-# from deepeval.test_case import LLMTestCase
+
 
 # Set up logging configuration with timestamps
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 def main(args):
-    test_files = f"data/intermediate/{args.source}/gpt35/{args.source}_test"
-    output_dir = f"data/output/{args.pipeline}-gpt35/{args.source}"
+    """
+    Main function to handle data loading, response generation, and metric evaluation.
+    """
+    question_file = f"data/training/data_partitioning/test/{args.question}.jsonl"
+    answer_file = f"data/training/data_partitioning/test/{args.answer}.csv"
+    output_dir = f"data/training/result/{args.output}"
 
-    openai = OpenAIConnection(args.model)
+    # Initialize OpenAI connection
+    openai = OpenAIConnection('gpt-3.5-turbo')
 
-    # logging.info(f"Loading test data; {args.n} records ...")
-    # tests = _load_test_data(test_files, args.n)
+    # Load test data from question and answer files
+    test_records = _load_test_data(question_file, answer_file)
 
-    # # logging.info("Generate response from base model...")
-    # # _generate_model_response(openai, tests, args.model, output_dir, f'base_{args.model}')
+    # Generate model's responses based on test records
+    logging.info(f"Generating responses for a total of {len(test_records)} records using model identifier: {args.id}.")
+    _generate_model_response(openai, test_records, args.id, output_dir)
 
-    # logging.info(f"Generate response from fine-tuned model...")
-    # for model_id in args.model_ids:
-    #     logging.info(f"Generate response from fine-tuned model: {model_id}...")
-    #     _generate_model_response(openai, tests, model_id, output_dir, f'{model_id.split(':')[-1]}')
-
-    _generate_metrics(output_dir)
+    # Generate performance metrics based on responses
+    logging.info(f"Generating Performance Metrics of responses obtained from model identifier: {args.id}.")
+    _generate_metrics(output_dir, args.id)
 
     
-def _load_test_data(test_files, n):
+def _load_test_data(question_file, answer_file):
+    """
+    Load and prepare test data from question and answer files.
+
+    Parameters:
+        question_file (str): Path to the JSONL file with question prompts.
+        answer_file (str): Path to the CSV file with true answers.
+
+    Returns:
+        list: A list of paired question prompts and true answers.
+    """
     test = []
-    with open(f"{test_files}_prompt.jsonl", 'r') as f1, open(f"{test_files}_answer.jsonl", 'r') as f2:
-        for i, (line_prompt, line_answer) in enumerate(zip(f1, f2)):
-            test.append([json.loads(line_prompt.strip())['messages'],line_answer.strip()])
-            if i+1 == n:
-                break
+    
+    # Load questions from JSONL
+    with open(question_file, 'r') as f1:
+        questions = [json.loads(line.strip())['messages'] for line in f1]
+
+    # Load answers from CSV
+    answers_df = pd.read_csv(answer_file, encoding='utf-8')
+    answers = answers_df['answers'].tolist()
+
+    # Zip questions and answers together
+    for i, (line_prompt, line_answer) in enumerate(zip(questions, answers)):
+        test.append([line_prompt, line_answer])
+
     return test
 
 
-def _generate_model_response(openai, tests, model, output_dir, type):
-    with open(f'{output_dir}/model_response__{type}.csv', mode='a', encoding='utf-8', newline='') as f:
+def _generate_model_response(openai, records, model, output_dir):
+    """
+    Generate model responses and save to a CSV file.
+
+    Parameters:
+        openai (OpenAIConnection): Connection to the OpenAI model.
+        records (list): List of question and answer pairs.
+        model (str): Model ID to use for generation.
+        output_dir (str): Directory to save the responses CSV.
+    """
+    with open(f'{output_dir}/responses/{model.split(':')[-1]}.csv', mode='a', encoding='utf-8', newline='') as f:
+
+        # Write header if the file is empty
         writer = csv.DictWriter(f, fieldnames=['prompt', 'true_answer', 'model_answer'])
         if f.tell() == 0:
             writer.writeheader()
-        for test in tests:
-            answer = openai._evaluate_model(model, test[0])
+
+        # Generate model responses and write them to CSV
+        for rec in records:
+            answer = openai._evaluate_model(model, rec[0])
             row = {
-                'prompt': test[0],
-                'true_answer': test[1],
+                'prompt': rec[0],
+                'true_answer': rec[1],
                 'model_answer': answer,
             }
             writer.writerow(row)
 
 
-def extract_context_and_question(prompt):
-    data_list = ast.literal_eval(prompt)
-    for item in data_list:
-        if item['role'] == 'user':
-            content = item['content']
-            if "Question:" in content:
-                question_part, context_part = content.split("Context:", 1)
-                question = question_part.strip().replace("Question:", "").strip()
-                context = context_part.strip()
-                    
-    return pd.Series([question, context])
+def _generate_metrics(output_dir, model):
+    """
+    Calculate and save evaluation metrics for model responses.
 
+    Parameters:
+        output_dir (str): Directory where the response CSV is located.
+        model (str): Model ID used for naming the metrics file.
+    """
+    inputfile = f'{output_dir}/responses/{model.split(':')[-1]}.csv'
+    outputfile = f'{output_dir}/metrics/{model.split(':')[-1]}.csv'
 
-def _generate_metrics(output_dir):
+    # Load BLEU and ROUGE scorers
     bleu = evaluate.load("bleu")
     scorer = rouge_scorer.RougeScorer(['rouge1', 'rouge2', 'rougeL', 'rougeLsum'], use_stemmer=True)
     
-    for file in os.listdir(output_dir):
-        if file.endswith('.csv'):
-            print(f"Processing {output_dir}/{file}")
-            df = pd.read_csv(f'{output_dir}/{file}')
-            bleu_list = []
-            rouge1_precision_list, rouge1_recall_list, rouge1_f1_list = [], [], []
-            rouge2_precision_list, rouge2_recall_list, rouge2_f1_list = [], [], []
-            rougeL_precision_list, rougeL_recall_list, rougeL_f1_list = [], [], []
-            rougeLSum_precision_list, rougeLSum_recall_list, rougeLSum_f1_list = [], [], []
+    # Read model responses from CSV
+    df = pd.read_csv(inputfile)
+    true_answers = df['true_answer'].tolist()
+    model_answers = df['model_answer'].tolist()
 
-            true_answers = df['true_answer'].tolist()
-            model_answers = df['model_answer'].tolist()
-            accuracy = accuracy_score(true_answers, model_answers)
-            precision = precision_score(true_answers, model_answers, average='weighted', zero_division=0)
-            recall = recall_score(true_answers, model_answers, average='weighted', zero_division=0)
-            f1 = f1_score(true_answers, model_answers, average='weighted', zero_division=0)
+    # Compute classification metrics
+    accuracy = accuracy_score(true_answers, model_answers)
+    precision = precision_score(true_answers, model_answers, average='weighted',zero_division=0)
+    recall = recall_score(true_answers, model_answers, average='weighted', zero_division=0)
+    f1 = f1_score(true_answers, model_answers, average='weighted', zero_division=0)
 
-            print(f"accuracy: {accuracy}")
-            print(f"precision: {precision}")
-            print(f"recall: {recall}")
-            print(f"f1: {f1}")
+    logging.info(f"accuracy: {accuracy}")
+    logging.info(f"precision: {precision}")
+    logging.info(f"recall: {recall}")
+    logging.info(f"f1: {f1}")
 
-            for index, row in df.iterrows():
-                true_answer = row['true_answer']
-                model_answer = row['model_answer']
+    # Compute BLEU scores
+    bleu_scores = [bleu.compute(predictions=[pred], references=[[ref]], max_order=2)['bleu']
+                   for pred, ref in zip(model_answers, true_answers)]
+    
+    # Initialize dictionaries to store ROUGE score components
+    rouge_scores = {
+        'rouge1_precision': [], 'rouge1_recall': [], 'rouge1_f1': [],
+        'rouge2_precision': [], 'rouge2_recall': [], 'rouge2_f1': [],
+        'rougeL_precision': [], 'rougeL_recall': [], 'rougeL_f1': [],
+        'rougeLSum_precision': [], 'rougeLSum_recall': [], 'rougeLSum_f1': []
+    }
 
-                bleu_result = bleu.compute(predictions=[model_answer], references=[[true_answer]],  max_order=2)
-                bleu_list.append(bleu_result['bleu'])
-                
-                scores = scorer.score(true_answer, model_answer)
+    # Calculate ROUGE scores and append results to the lists
+    for true, pred in zip(true_answers, model_answers):
+        scores = scorer.score(true, pred)
+        rouge_scores['rouge1_precision'].append(scores['rouge1'].precision)
+        rouge_scores['rouge1_recall'].append(scores['rouge1'].recall)
+        rouge_scores['rouge1_f1'].append(scores['rouge1'].fmeasure)
 
-                rouge1_precision_list.append(scores['rouge1'].precision)
-                rouge1_recall_list.append(scores['rouge1'].recall)
-                rouge1_f1_list.append(scores['rouge1'].fmeasure)
-                
-                rouge2_precision_list.append(scores['rouge2'].precision)
-                rouge2_recall_list.append(scores['rouge2'].recall)
-                rouge2_f1_list.append(scores['rouge2'].fmeasure)
+        rouge_scores['rouge2_precision'].append(scores['rouge2'].precision)
+        rouge_scores['rouge2_recall'].append(scores['rouge2'].recall)
+        rouge_scores['rouge2_f1'].append(scores['rouge2'].fmeasure)
 
-                rougeL_precision_list.append(scores['rougeL'].precision)
-                rougeL_recall_list.append(scores['rougeL'].recall)
-                rougeL_f1_list.append(scores['rougeL'].fmeasure)
+        rouge_scores['rougeL_precision'].append(scores['rougeL'].precision)
+        rouge_scores['rougeL_recall'].append(scores['rougeL'].recall)
+        rouge_scores['rougeL_f1'].append(scores['rougeL'].fmeasure)
 
-                rougeLSum_precision_list.append(scores['rougeLsum'].precision)
-                rougeLSum_recall_list.append(scores['rougeLsum'].recall)
-                rougeLSum_f1_list.append(scores['rougeLsum'].fmeasure)
-                
-            df['bleu'] = bleu_list
-            df['rouge1_precision'] = rouge1_precision_list
-            df['rouge1_recall'] = rouge1_recall_list
-            df['rouge1_f1'] = rouge1_f1_list
-            df['rouge2_precision'] = rouge2_precision_list
-            df['rouge2_recall'] = rouge2_recall_list
-            df['rouge2_f1'] = rouge2_f1_list
-            df['rougeL_precision'] = rougeL_precision_list
-            df['rougeL_recall'] = rougeL_recall_list
-            df['rougeL_f1'] = rougeL_f1_list
-            df['rougeLSum_precision'] = rougeLSum_precision_list
-            df['rougeLSum_recall'] = rougeLSum_recall_list
-            df['rougeLSum_f1'] = rougeLSum_f1_list
+        rouge_scores['rougeLSum_precision'].append(scores['rougeLsum'].precision)
+        rouge_scores['rougeLSum_recall'].append(scores['rougeLsum'].recall)
+        rouge_scores['rougeLSum_f1'].append(scores['rougeLsum'].fmeasure)
 
-            df.to_csv(f'{output_dir}/records_metrics_{file}', index=False)
+    # Append BLEU and ROUGE scores to DataFrame
+    df['bleu'] = bleu_scores
+    for key, values in rouge_scores.items():
+        df[key] = values
+
+    # Save the metrics to output CSV
+    df.to_csv(outputfile, index=False)
 
 
 def _setup_args():
+    """
+    Set up command-line arguments.
+
+    Returns:
+        argparse: The parsed arguments.
+    """
     parser = argparse.ArgumentParser()
-    parser.add_argument('model', type=str, default='gpt-3.5-turbo', nargs='?', help='Model Type')
-    parser.add_argument('--model_ids', type=str, nargs='*', help='Model IDs')
-    parser.add_argument('--n', type=int, default=1000, help='Model ID')
-    parser.add_argument('--source', type=str, default='squad', help='Type of Data')
-    parser.add_argument('--pipeline', type=str, default='custom', help='Type of Data')
+    parser.add_argument('id', type=str, nargs='?', help='Model ID')
+    parser.add_argument('-question', type=str, help='File containing prompts')
+    parser.add_argument('-answer', type=str, help='File containing answers')
+    parser.add_argument('-output', type=str, help='Output Filepath')
     args = parser.parse_args()
 
     return args
 
 
 if __name__ == "__main__":
+    # Set up command-line arguments
     args = _setup_args()
+
+    # Execute the main function with the parsed arguments
     main(args)
